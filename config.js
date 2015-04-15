2f29b4ce8b6a20b6584b33dece6ac4cce1ffe412
Catalin Iosif <ciosifweb@gmail.com>
2015-04-15 12:28:58 -0400

    Initial project with server and signup API

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+	"database": "mongodb://root:fullstack@localhost:27017/userStory",
+	"port": process.env.port || 3000,
+	"secretKey": "3littlehoods"
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+var mongoose = require("mongoose");
+var config = require("./config");
+
+var app = express();
+
+//connect to database
+mongoose.connect(config.database, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Connected to db");
+	}
+})
+
+//middleware for parsing urls, json, and for logging requests to the console
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+var api = require('./app/routes/api')(app, express);
+app.use('/api', api);
+
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/public/views/index.html');
+});
+
+app.listen(config.port, function(err) {
+
+	if (err) {
+		console.log(err);
+	} else
+	{
+		console.log("Listening on port 3000...")
+	}
+});
