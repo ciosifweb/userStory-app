5c90626958ccaa8154bfc9307af8235d0c91acd6
Catalin Iosif <ciosifweb@gmail.com>
2015-04-16 13:50:24 -0400

    Added ability to post and retrieve user stories

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,12 +1,15 @@
 var User = require("../models/user");
+var Story = require("../models/story");
 var config = require("../../config");
 var jsonwebtoken = require("jsonwebtoken");
 var secretKey = config.secretKey;
 
+//create login token
+
 function createToken(user) {
 
 	var token = jsonwebtoken.sign({
-		_id: user._id,
+		id: user._id,
 		name: user.name,
 		username: user.username
 	}, secretKey, {
@@ -16,8 +19,12 @@ function createToken(user) {
 	return token;
 }
 
+// express module
+
 module.exports = function(app, express) {
 	var api = express.Router();
+
+	// signup route
 
 	api.post('/signup', function(req, res) {
 		var user = new User({
@@ -36,6 +43,8 @@ module.exports = function(app, express) {
 		});
 	});
 
+	//display users route
+
 	api.get('/users', function(req, res) {
 		User.find({}, function(err, users) {
 			if (err) {
@@ -47,8 +56,11 @@ module.exports = function(app, express) {
 		});
 	});
 
+	//login route
+
 	api.post('/login', function(req, res) {
 
+		//find the user according to the username, and get the password from the response
 		User.findOne({
 			username: req.body.username
 		 }).select('password').exec(function(err, user) {
@@ -78,6 +90,8 @@ module.exports = function(app, express) {
 		 })
 	});
 
+	//middleware that checks if the user has logged in
+
 	api.use(function(req, res, next) {
 		console.log("Someone just connected to our app!");
 
@@ -99,9 +113,35 @@ module.exports = function(app, express) {
 		}
 	});
 
-	api.get('/', function(req, res) {
-		res.json("Hello World");
-	});
+	// after token check middleware
+
+	//post and get user stories on the root path
+
+	api.route('/')
+		.post(function(req, res) {
+			var story = new Story({
+				creator: req.decoded.id,
+				content: req.body.content,
+			});
+			story.save(function(err) {
+				if (err) {
+					res.send(err);
+					return;
+				}
+				res.json({message: "New story created"});
+			});
+		})
+
+		.get(function(req, res) {
+
+			Story.find({creator: req.decoded.id}, function(err, story) {
+				if (err) {
+					res.send(err);
+					return;
+				}
+				res.json(story)
+			})
+		})
 
 	return api;
 };
