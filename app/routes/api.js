06189848fea885697af0662b5904f2ead1aeac61
Catalin Iosif <ciosifweb@gmail.com>
2015-04-16 13:12:54 -0400

    Added middleware for home page functionality

--- app/routes/api.js
+++ app/routes/api.js
@@ -78,5 +78,30 @@ module.exports = function(app, express) {
 		 })
 	});
 
+	api.use(function(req, res, next) {
+		console.log("Someone just connected to our app!");
+
+		var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+		//check if token exits
+
+		if(token) {
+			jsonwebtoken.verify(token, secretKey, function(err, decoded) {
+				if (err) {
+					res.status(403).send({success: false, message: "Failed to authenticate"});
+
+				} else {
+					req.decoded = decoded;
+					next();
+				}
+			});
+		} else {
+			res.status(403).send({success: false, message: "No Token Provided"});
+		}
+	});
+
+	api.get('/', function(req, res) {
+		res.json("Hello World");
+	});
+
 	return api;
 };
